12669642870e9fc11e0f60639d8641a9a14f99de
tete-30 <hortensia_porto@hotmail.com>
2023-11-29 22:09:37 -0300

    Initial commit 291123

---
+++ src/main/java/cevaja/CevajaApplication.java
@@ -0,0 +1,13 @@
+package cevaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CevajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CevajaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cevaja/CevajaApplicationTests.java
@@ -0,0 +1,13 @@
+package cevaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CevajaApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
